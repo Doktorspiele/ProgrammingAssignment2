2243dfbadd88e39e3fdf99240932847d2627efe3
Doktorspiele <bartfetlinski@gmail.com>
2016-08-08 12:39:34 +0200

    2nd assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Set of functions for solving the second assigment. Focus on clarity rather than code volume minimization.
 
 ## Write a short comment describing this function
+
+## This function caches the inverted matrix, enables passing the input matrix and returns list of access functions, 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  #Flag to check if solution already exists + access and setting functions
+  cacheExists<-FALSE 
+  getFlag<-function() cacheExists
+  setFlag<-function(value) cacheExists<<-value # <<- required because we want to update makeCacheMatrix from inside of cacheSolve
+  
+  #function to pass input argument
+  getData<-function() x
+  
+  #variable for storage of the solution + access and setting functions
+  cachedMatrix<-NULL
+  getCache<-function() cachedMatrix
+  setCache<-function(matr) cachedMatrix<<-matr # again, <<- needed becouse we seet this value out of scope of this function
+  
+  #returned list of access functions
+  list(getFlag=getFlag,setFlag=setFlag, getCache=getCache, setCache=setCache, getData=getData)
 }
 
-
-## Write a short comment describing this function
+## This function takes as input the output from the makeCacheMatrix() function. Checks if the inverted matrix has already been 
+## calculated for given input, if yes returns cached result, if not calculates the inverted matrix, updates cache and sets the flag
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  if (x$getFlag()) 
+  {
+    print("taking cached matrix")
+    x$getCache()
+    return(x$getCache())
+  }
+  
+  temp<-x$getData()
+  result<-solve(temp)
+  x$setCache(result)
+  x$setFlag(TRUE)
+  result
+  return(result)
+  
+  ## Return a matrix that is the inverse of 'x'
 }
+
+
